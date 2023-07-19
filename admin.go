2db32bddadf0ffc89180222ab42600dacfbed0b0
Jad <jadbsaid@gmail.com>
2023-07-19 10:58:51 -0300

    added factory reset method

--- admin.go
+++ admin.go
@@ -27,6 +27,26 @@ func (c *CableModem) CMReboot(ctx context.Context) (*Error, error) {
 	return &o, nil
 }
 
+func (c *CableModem) CMReset(ctx context.Context) (*Error, error) {
+	csrf, err := c.UsersCSRF(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to retrieve CSRF token: %w", err)
+	}
+
+	o := Error{}
+
+	err = c.sendRequest(ctx, http.MethodPost, "/CM/Reboot",
+		url.Values{
+			"model": []string{`{"factoryReset":1}`},
+			"csrf":  []string{csrf.CSRF},
+		}, &o)
+	if err != nil {
+		return nil, err
+	}
+
+	return &o, nil
+}
+
 func (c *CableModem) CMClearLog(ctx context.Context) (*Error, error) {
 	csrf, err := c.UsersCSRF(ctx)
 	if err != nil {
