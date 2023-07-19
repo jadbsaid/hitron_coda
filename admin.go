f2f2bfa27862c51af0e79119cd422b21f5746054
Jad <jadbsaid@gmail.com>
2023-07-19 11:07:21 -0300

    feat: easy connect setup

--- admin.go
+++ admin.go
@@ -67,3 +67,18 @@ func (c *CableModem) CMClearLog(ctx context.Context) (*Error, error) {
 
 	return &o, nil
 }
+
+// SelfInstall initializes the modem by completing the Easy Connect setup wizard
+func (c *CableModem) SelfInstall(ctx context.Context) (*Error, error) {
+	o := Error{}
+
+	err := c.sendRequest(ctx, http.MethodPost, "/SelfInstall",
+		url.Values{
+			"model": []string{fmt.Sprintf(`{"name":"%s","password":"%s"}`, c.credentials.Username, c.credentials.Password)},
+		}, o)
+	if err != nil {
+		return nil, err
+	}
+
+	return &o, nil
+}
